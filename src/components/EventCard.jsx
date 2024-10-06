7fdd33b7cc78c675d602e7fc64cd7435880f00e1
Michipache21 <ricardo.calderon4@unmsm.edu.pe>
2024-10-05 19:56:49 -0500

    EventCard

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -6,12 +6,13 @@ import ScheduleIcon from '@mui/icons-material/Schedule';
 
 const events = [
   { date: '05 set', title: 'Inauguración del semestre 2024-2 para los ingresantes de estudios de posgrado en la Facultad de Educación' },
-  { date: '07 ago', title: 'Programa de formación de Auxiliares en Educación para los niveles de Inicial y Primaria' },
+  { date: '07 ago', title: 'Programa de formación Ingenier' },
   { date: '27 jun', title: 'Conferencias virtuales por el Día del Maestro' },
   { date: '13 jun', title: 'USO DE HERRAMIENTAS DE INTELIGENCIA DE ARTIFICIAL GENERATIVA' },
   { date: '13 jun', title: 'Technology, Artificial Intelligence and Digital Materials in ELT: Specialisation Course' },
+  { date: '15 oct', title: 'Taller de Desarrollo Ágil: Scrum en la Práctica' },
+  { date: '22 oct', title: 'Conferencia sobre Seguridad en Aplicaciones Web' },
 ];
-
 const EventSection = () => {
   return (
     <Box sx={{ p: 4 }}>
@@ -47,11 +48,11 @@ const EventSection = () => {
               <Box
                 component="img"
                 sx={{ width: '100%', borderRadius: '8px', mt: 2 }}
-                src="https://via.placeholder.com/600x200" // Puedes reemplazarlo por tu imagen
+                src="/src/assets/fisi2.jpg" // Puedes reemplazarlo por tu imagen
                 alt="Evento destacado"
               />
               <Typography variant="h6" sx={{ mt: 2 }}>
-                Inauguración del semestre 2024-2 para los ingresantes de estudios de posgrado en la Facultad de Educación
+                Inauguración del semestre 2024-2 para los ingresantes de estudios de posgrado en la Facultad de Ingenieria de sistemas e informatica
               </Typography>
               <Typography variant="body2" sx={{ color: 'gray', mt: 1 }}>
                 Del 05 al 07 de septiembre de 2024
@@ -59,7 +60,7 @@ const EventSection = () => {
 
               <Box sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
                 <LocationOnIcon sx={{ mr: 1, color: 'gray' }} />
-                <Typography variant="body2" sx={{ color: 'gray' }}>Auditorio Luis Miró Quesada de la Guerra</Typography>
+                <Typography variant="body2" sx={{ color: 'gray' }}>Auditorio de la FISI</Typography>
               </Box>
               <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
                 <ScheduleIcon sx={{ mr: 1, color: 'gray' }} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,12 @@
 import '../styles/App.css'; // Para importar App.css
-import CardComponent from '../components/Card'; // Asegúrate de que la importación sea correcta
-import AutoPlay from '../components/Carousel/Carousel'; // Asegúrate de que carousel.jsx exista en la carpeta Carousel
 import { Container, Box, Grid, Typography } from '@mui/material'; // Importar solo lo necesario de Material UI
+import AutoPlay from '../components/Carousel/Carousel'; // Asegúrate de que carousel.jsx exista en la carpeta Carousel
+import CardComponent from '../components/Card'; // Asegúrate de que la importación sea correcta
 import NewsCard from '../components/Newscard'; // Importar el nuevo componente
 import { cardData } from '../components/Card'; // Asegúrate de que cardData esté definido en card.js
-<<<<<<< HEAD:src/pages/Home.jsx
-import EventSection from '../components/EventCard'; // Importa el nuevo componente EventCardList
-function Home() {
-=======
 import EventCardList from '../components/EventCard'; // Importa el nuevo componente EventCardList
 
 function HomePage() {
->>>>>>> aaecb270feb3cbd3ae21bbea5d80bb6df834f704:src/pages/HomePage.jsx
     return (
         <>
             <AutoPlay />
@@ -30,14 +25,15 @@ function HomePage() {
                     </Grid>
                 </Box>
 
-                <Typography variant="h4" sx={{ textAlign: 'center',   marginBottom: 2, color: '#FFFFFF', fontWeight: 'bold' }}>
+                <Typography variant="h4" sx={{ textAlign: 'center', marginBottom: 2,fontWeight: 'bold' }}>
                     NOTICIAS
                 </Typography>
-                <NewsCard/> 
-                <Typography variant="h4" sx={{ textAlign: 'center',   marginBottom: 2, color: '#FFFFFF', fontWeight: 'bold' }}>
+                <NewsCard /> 
+
+                <Typography variant="h4" sx={{ textAlign: 'center', marginBottom: 2 ,fontWeight: 'bold' }}>
                     EVENTOS
                 </Typography>          
-                <EventSection /> {/* Añade el componente EventCardList aquí */}     
+                <EventCardList /> {/* Añade el componente EventCardList aquí */}     
             </Container>
         </>
     );
