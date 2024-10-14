2299ea6c29994a6102f957c6d15930fa9887252b
Mateo Choy <mateo.choy@unmsm.edu.pe>
2024-10-14 09:57:19 -0500

    Data para rectificaciones

--- src/pages/Rectis/Rectificaciones.jsx
+++ src/pages/Rectis/Rectificaciones.jsx
@@ -22,38 +22,214 @@ import TextField from '@mui/material/TextField';
 // Datos JSON
 const initialData = [
   {
-    id: 1,
-    apellido: "García",
-    nombre: "Juan",
-    code: 1001,
-    promedio: 15.75,
-    cursoSalida: "Curso 3",
-    cursoEntrada: "Curso 4",
-    fecha: "2024-10-15T10:00:00Z",
-    tipo: "Entrada"
-  },
-  {
-    id: 2,
-    apellido: "Martínez",
-    nombre: "Lucía",
-    code: 1002,
-    promedio: 18.90,
-    cursoSalida: "Curso 2",
-    cursoEntrada: "Curso 3",
-    fecha: "2024-09-07T09:00:00Z",
+    id: 101,
+    apellido: "ABAD HUAMAN",
+    nombre: "FRANCISCO JAVIER",
+    code: 20200111,
+    promedio: 15.11,
+    cursoSalida: "Medio Ambiente y Desarrollo Sostenible",
+    cursoEntrada: "Medio Ambiente y Desarrollo Sostenible",
+    fecha: "2024-10-14T09:32:40Z",
     tipo: "Cambio"
-  },
-  {
-    id: 3,
-    apellido: "Fernández",
-    nombre: "Carlos",
-    code: 1003,
-    promedio: 17.45,
-    cursoSalida: "Curso 1",
-    cursoEntrada: "Curso 2",
-    fecha: "2024-06-27T14:00:00Z",
+},
+{
+    id: 102,
+    apellido: "ABAL CARHUANCHO",
+    nombre: "PAOLA RAYZA",
+    code: 22200067,
+    promedio: 17.64,
+    cursoSalida: "Algoritmica I",
+    cursoEntrada: "Algotimica I",
+    fecha: "2024-10-14T09:32:40Z",
+    tipo: "Cambio"
+},
+{
+    id: 103,
+    apellido: "ABANTO RUEDA",
+    nombre: "REYNERIO INOCENCIO",
+    code: 21207177,
+    promedio: 15.18,
+    cursoSalida: "",
+    cursoEntrada: "Desarrollo Movil",
+    fecha: "2024-10-14T09:32:41Z",
+    tipo: "Entrada"
+},
+{
+    id: 104,
+    apellido: "ABANTO SALAS",
+    nombre: "FLAVIA FRANCESCA",
+    code: 19200064,
+    promedio: 16.45,
+    cursoSalida: "Calculo II",
+    cursoEntrada: "",
+    fecha: "2024-10-14T09:32:42Z",
     tipo: "Salida"
-  }
+},
+{
+    id: 105,
+    apellido: "ABARCA ARANDA",
+    nombre: "JOSE LUIS",
+    code: 22200001,
+    promedio: 14.67,
+    cursoSalida: "Interaccion Humano Computador",
+    cursoEntrada: "Interaccion Humano Computador",
+    fecha: "2024-10-14T09:32:42Z",
+    tipo: "Cambio"
+},
+{
+    id: 106,
+    apellido: "ABARCA RAMOS",
+    nombre: "ALBERTO JUNIOR",
+    code: 16200001,
+    promedio: 16.81,
+    cursoSalida: "Matematica Basica",
+    cursoEntrada: "Matematica Basica",
+    fecha: "2024-10-14T09:32:43Z",
+    tipo: "Cambio"
+},
+{
+    id: 107,
+    apellido: "ACOSTA GIBAJA",
+    nombre: "RODRIGO YAMIL",
+    code: 19200241,
+    promedio: 14.32,
+    cursoSalida: "Base de Datos II",
+    cursoEntrada: "",
+    fecha: "2024-10-14T09:32:43Z",
+    tipo: "Salida"
+},
+{
+    id: 108,
+    apellido: "ACOSTA HUARCAYA",
+    nombre: "NICOLLE",
+    code: 19200131,
+    promedio: 15.43,
+    cursoSalida: "",
+    cursoEntrada: "Matetatica Discreta",
+    fecha: "2024-10-14T09:32:44Z",
+    tipo: "Entrada"
+},
+{
+    id: 109,
+    apellido: "ACUÑA ANAMPA",
+    nombre: "BRITNEY JENNIFER",
+    code: 18200129,
+    promedio: 12.36,
+    cursoSalida: "Estructura de Datos",
+    cursoEntrada: "",
+    fecha: "2024-10-14T09:32:44Z",
+    tipo: "Salida"
+},
+{
+    id: 110,
+    apellido: "ACUÑA CORDOVA",
+    nombre: "DAVIS BRETTS",
+    code: 21207074,
+    promedio: 16.39,
+    cursoSalida: "Sistemas Operativos",
+    cursoEntrada: "Sistemas Operativos",
+    fecha: "2024-10-14T09:32:45Z",
+    tipo: "Cambio"
+},
+{
+    id: 111,
+    apellido: "ACUÑA MONTALVAN",
+    nombre: "GERALDINE DAYHANA",
+    code: 22200185,
+    promedio: 17.46,
+    cursoSalida: "Analisis y Diseño de Algoritmos",
+    cursoEntrada: "",
+    fecha: "2024-10-14T09:32:45Z",
+    tipo: "Salida"
+},
+{
+    id: 112,
+    apellido: "ADCO VALERIANO",
+    nombre: "DIANA YANETH",
+    code: 21207087,
+    promedio: 0,
+    cursoSalida: "",
+    cursoEntrada: "Programacion Paralela",
+    fecha: "2024-10-14T09:32:46Z",
+    tipo: "Entrada"
+},
+{
+    id: 113,
+    apellido: "ADOLFO PAUCAR",
+    nombre: "KILTOM",
+    code: 22200244,
+    promedio: 15.75,
+    cursoSalida: "Fisica Electronica",
+    cursoEntrada: "Fisica Electronica",
+    fecha: "2024-10-14T09:32:47Z",
+    tipo: "Cambio"
+},
+{
+    id: 114,
+    apellido: "AGREDA ROMERO",
+    nombre: "CHRISTIAN OSWALDO",
+    code: 21207080,
+    promedio: 13.14,
+    cursoSalida: "Internet de las Cosas",
+    cursoEntrada: "",
+    fecha: "2024-10-14T09:32:47Z",
+    tipo: "Salida"
+},
+{
+    id: 115,
+    apellido: "AGUILAR BURGA",
+    nombre: "PIERO ANDRÉ",
+    code: 16200203,
+    promedio: 12.56,
+    cursoSalida: "Machine Learning",
+    cursoEntrada: "Machine Learning",
+    fecha: "2024-10-14T09:32:48Z",
+    tipo: "Cambio"
+},
+{
+    id: 116,
+    apellido: "AGUILAR CAMPOS",
+    nombre: "ESTEFANY SILVIA",
+    code: 17200129,
+    promedio: 15.43,
+    cursoSalida: "Probabilidades",
+    cursoEntrada: "Probabilidades",
+    fecha: "2024-10-14T09:32:48Z",
+    tipo: "Cambio"
+},
+{
+    id: 117,
+    apellido: "AGUILAR ESPINOZA",
+    nombre: "JOEL ARMANDO",
+    code: 18200054,
+    promedio: 14.39,
+    cursoSalida: "",
+    cursoEntrada: "Estadistica",
+    fecha: "2024-10-14T09:32:49Z",
+    tipo: "Entrada"
+},
+{
+    id: 118,
+    apellido: "AGUILAR MATA",
+    nombre: "CRISBEL LEIDY",
+    code: 21200020,
+    promedio: 16.17,
+    cursoSalida: "Gestion de Empresas de Software",
+    cursoEntrada: "Gestion de Empresas de Software",
+    fecha: "2024-10-14T09:32:50Z",
+    tipo: "Cambio"
+},
+{
+    id: 119,
+    apellido: "AGUILAR PAREDES",
+    nombre: "NICOLÁS MARTÍN",
+    code: 17200130,
+    promedio: 0,
+    cursoSalida: "",
+    cursoEntrada: "Sistemas Distribuidos",
+    fecha: "2024-10-14T09:32:50Z",
+    tipo: "Entrada"
+}
 ];
 
 const columns = [
